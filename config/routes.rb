46f8383dcc555507290bffaf3cd09a443d7b90bb
gobbleclark <gavingclark4@gmail.com>
2021-06-01 17:27:12 -0600

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
+
+  namespace :api do
+    resources :posts
+  end
+  
+  
+  get '*other', to: 'static#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200103184409) do
+ActiveRecord::Schema.define(version: 20210601231723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
